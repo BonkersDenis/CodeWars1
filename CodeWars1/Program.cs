2b5149978e535a09c13e4e997ed15f50f17d8fed
Nikit0chka <voronsov.nikita19742004@gmail.com>
2024-03-26 21:35:46 +0400

    Alo

--- CodeWars1/Program.cs
+++ CodeWars1/Program.cs
@@ -8,20 +8,20 @@ namespace CodeWars1
    
     internal class Program
     {
-        public static int FindDup(int[] arr)
-        {
-            int duplicateValue = 0;
-            Array.Sort(arr);
-           for (int i = 0;i<arr.Length-1;i++)
-            {
-                if (arr[i] == arr[i+1])
-                {
-                    duplicateValue = arr[i];
-                }
-
-            }
-            return duplicateValue;
-        }
+        // public static int FindDup(int[] arr)
+        // {
+        //     int duplicateValue = 0;
+        //     Array.Sort(arr);
+        //    for (int i = 0;i<arr.Length-1;i++)
+        //     {
+        //         if (arr[i] == arr[i+1])
+        //         {
+        //             duplicateValue = arr[i];
+        //         }
+        //
+        //     }
+        //     return duplicateValue;
+        // }
 
 
         static void Main(string[] args)
