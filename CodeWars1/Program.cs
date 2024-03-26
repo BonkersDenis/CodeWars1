ee22ada7d66b20226ae840e8b08e712dec70d9eb
Denis <Denis@DESKTOP-MGTPBOP>
2024-03-26 20:44:20 +0300

    Добавьте файлы проекта.

---
+++ CodeWars1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+
+namespace CodeWars1
+{
+
+   
+    internal class Program
+    {
+        public static int FindDup(int[] arr)
+        {
+            int duplicateValue = 0;
+            Array.Sort(arr);
+           for (int i = 0;i<arr.Length-1;i++)
+            {
+                if (arr[i] == arr[i+1])
+                {
+                    duplicateValue = arr[i];
+                }
+
+            }
+            return duplicateValue;
+        }
+
+
+        static void Main(string[] args)
+        {
+             ConsoleWork number = new ConsoleWork();
+            
+           
+
+        }
+    }
+}
---
+++ CodeWars1/Triangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars1
+{
+    
+    public class Triangle 
+    {
+        private double _sideA;
+        private double _sideB;
+        private double _sideC;
+        public double AngleAB;
+        public double AngleBC;
+        public double AngleCA;
+        public string Name;
+        
+        public Triangle(string name) {  Name = name; }
+        
+        public Triangle(double sideA, double sideB, double sideC, double angleAB, double angleBC, double angleCA, string name)
+        {
+            if (IsCanCreate(sideA, sideB, sideC))
+            {
+                
+                _sideA = sideA;
+                _sideB = sideB;
+                _sideC = sideC;
+                AngleAB = angleAB;
+                AngleBC = angleBC;
+                AngleCA = angleCA;
+                Name = name;
+                
+            }
+            else
+            {
+                throw new Exception($"Невозможно создать треугольник со сторонами:{sideA}, {sideB}, {sideC}");
+            }
+        }
+
+       public void SetSides(double sideA, double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+
+        }
+        public string Information() 
+        {
+            return $"side A = {_sideA}\n side B = {_sideB}\n side C = {_sideC}\n Name: {Name} ";
+            
+        }
+        private bool IsCanCreate(double sideA,double sideB,double sideC) 
+        {
+            return ((sideA + sideB > sideC) && (sideB + sideC > sideA) && (sideC + sideA > sideB));
+            
+        }
+       
+    }
+}
